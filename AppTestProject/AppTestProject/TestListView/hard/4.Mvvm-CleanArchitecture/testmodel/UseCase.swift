7b56be3addaa1552c8d4631f74bf01a4f9d63151
lunight <kimkshahaha@gmail.com>
2021-03-09 21:17:33 +0900

    work

--- AppTestProject/AppTestProject/TestListView/hard/4.Mvvm-CleanArchitecture/testmodel/UseCase.swift
+++ AppTestProject/AppTestProject/TestListView/hard/4.Mvvm-CleanArchitecture/testmodel/UseCase.swift
@@ -10,48 +10,48 @@ import Foundation
 
 final class UserDefaultsInfoQueriesStore {
     
-    private let recentsQuesryKey = "key"
-    private var userDefaults: UserDefaults
-    private let mainBundle = Bundle.main
-    
-    init(userDefaults: UserDefaults = UserDefaults.standard) {
-        
-        self.userDefaults = userDefaults
-    }
-    
-    private func setRequestValue<T>(value: T, key: String) {
-        
-    }
-    private func setRequest<T>(value: T , key: String) {
-        userDefaults.set(value, forKey: key)
-    }
-    
-    
-    private func getRequest(key: String) -> Any{
-        return userDefaults.object(forKey: key) ?? ""
-    }
-    
-    
-    /// 키에 해당하는 데이터 삭제
-    /// - Parameter key: 키
-    private func removeRequest(key: String) {
-        userDefaults.removeObject(forKey:key)
-    }
-    
-    /// UserDefaults에 저장된 모든 사용자  데이터 제거
-    private func clearUserDataRequest() {
-        if let bundleId = mainBundle.bundleIdentifier {
-            userDefaults.removeVolatileDomain(forName: bundleId)
-        }
-        
-    }
-    
-    /// UserDefaults에 저장된 모든 데이터 제거(앱초기화)
-    private func clearAppDataRequest() {
-        if let bundleId = mainBundle.bundleIdentifier {
-            userDefaults.removePersistentDomain(forName: bundleId)
-        }
-    }
+//    private let recentsQuesryKey = "key"
+//    private var userDefaults: UserDefaults
+//    private let mainBundle = Bundle.main
+//    
+//    init(userDefaults: UserDefaults = UserDefaults.standard) {
+//        
+//        self.userDefaults = userDefaults
+//    }
+//    
+//    private func setRequestValue<T>(value: T, key: String) {
+//        
+//    }
+//    private func setRequest<T>(value: T , key: String) {
+//        userDefaults.set(value, forKey: key)
+//    }
+//    
+//    
+//    private func getRequest(key: String) -> Any{
+//        return userDefaults.object(forKey: key) ?? ""
+//    }
+//    
+//    
+//    /// 키에 해당하는 데이터 삭제
+//    /// - Parameter key: 키
+//    private func removeRequest(key: String) {
+//        userDefaults.removeObject(forKey:key)
+//    }
+//    
+//    /// UserDefaults에 저장된 모든 사용자  데이터 제거
+//    private func clearUserDataRequest() {
+//        if let bundleId = mainBundle.bundleIdentifier {
+//            userDefaults.removeVolatileDomain(forName: bundleId)
+//        }
+//        
+//    }
+//    
+//    /// UserDefaults에 저장된 모든 데이터 제거(앱초기화)
+//    private func clearAppDataRequest() {
+//        if let bundleId = mainBundle.bundleIdentifier {
+//            userDefaults.removePersistentDomain(forName: bundleId)
+//        }
+//    }
     
 }
 
